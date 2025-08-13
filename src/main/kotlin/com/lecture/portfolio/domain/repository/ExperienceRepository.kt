8168c9f4a42eeef40d9e75939a218873a04e591e
WEBNET_DOM\jpk6327 <jpk6327@fashionplus.co.kr>
2025-08-13 20:54:45 +0900

    테스트 코드 작성(ExperienceRepositoryTest)

--- src/main/kotlin/com/lecture/portfolio/domain/repository/ExperienceRepository.kt
+++ src/main/kotlin/com/lecture/portfolio/domain/repository/ExperienceRepository.kt
@@ -2,8 +2,10 @@ package com.lecture.portfolio.domain.repository
 
 import com.lecture.portfolio.domain.entity.Experience
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
 
 interface ExperienceRepository : JpaRepository<Experience, Long> {
 
     fun findAllByIsActive(isActive: Boolean): List<Experience>
+
 }
---
+++ src/test/kotlin/com/lecture/portfolio/domain/repository/ProjectRepositoryTest.kt
@@ -0,0 +1,4 @@
+package com.lecture.portfolio.domain.repository
+
+class ProjectRepositoryTest {
+}
