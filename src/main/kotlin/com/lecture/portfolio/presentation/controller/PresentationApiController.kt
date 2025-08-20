1b43b8e5f20f0d727f82dc490cf0ac3140266700
WEBNET_DOM\jpk6327 <jpk6327@fashionplus.co.kr>
2025-08-20 20:15:16 +0900

    컨트롤러 개발

--- src/main/kotlin/com/lecture/portfolio/presentation/controller/PresentationApiController.kt
+++ src/main/kotlin/com/lecture/portfolio/presentation/controller/PresentationApiController.kt
@@ -1,15 +1,42 @@
 package com.lecture.portfolio.presentation.controller
 
+import com.lecture.portfolio.presentation.dto.IntroductionDTO
+import com.lecture.portfolio.presentation.dto.LinkDTO
+import com.lecture.portfolio.presentation.dto.ProjectDTO
+import com.lecture.portfolio.presentation.dto.ResumeDTO
+import com.lecture.portfolio.presentation.service.PresentationService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
-class PresentationApiController {
+class PresentationApiController (
+    private val presentationService: PresentationService
+){
 
     @GetMapping("/health-check")
     fun healthCheck(): String {
         return "Health check OK"
+    }
+
+    @GetMapping("/v1/introductions")
+    fun getIntroductions(): List<IntroductionDTO> {
+        return presentationService.getIntroductions()
+    }
+
+    @GetMapping("/v1/links")
+    fun getLinks(): List<LinkDTO> {
+        return presentationService.getLinks()
+    }
+
+    @GetMapping("/v1/resume")
+    fun getResume(): ResumeDTO {
+        return presentationService.getResume()
+    }
+
+    @GetMapping("/v1/projects")
+    fun getProjects(): List<ProjectDTO> {
+        return presentationService.getProjects()
     }
 }
--- src/main/kotlin/com/lecture/portfolio/presentation/controller/PresentationViewController.kt
+++ src/main/kotlin/com/lecture/portfolio/presentation/controller/PresentationViewController.kt
@@ -1,14 +1,48 @@
 package com.lecture.portfolio.presentation.controller
 
+import com.lecture.portfolio.domain.constant.SkillType
+import com.lecture.portfolio.presentation.service.PresentationService
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class PresentationViewController {
+class PresentationViewController(
+    private val presentationService: PresentationService
+) {
     @GetMapping("test")
     fun test(): String {
         // resources - templates - test.html을 찾아 보여준다.
         // return "test" => test.html로 이름이 같아야한다.
         return "test"
+    }
+
+    @GetMapping("/")
+    fun index(model: Model) : String {
+        val introductions = presentationService.getIntroductions()
+        val links = presentationService.getLinks()
+
+        model.addAttribute("introductions", introductions)
+        model.addAttribute("links", links)
+        return "presentation/index"
+    }
+
+    @GetMapping("/resume")
+    fun resume(model: Model) : String {
+        val resume = presentationService.getResume()
+
+        model.addAttribute("resume", resume)
+        model.addAttribute("skillTypes", SkillType.values())
+
+        return "presentation/resume"
+    }
+
+    @GetMapping("/projects")
+    fun projects(model: Model) : String {
+        val projects = presentationService.getProjects()
+
+        model.addAttribute("projects", projects)
+
+        return "presentation/projects"
     }
 }
